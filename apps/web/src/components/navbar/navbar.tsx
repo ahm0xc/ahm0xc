fe2e5acde13e5ced9ef358e670a972e9dac4835d
Ahmed <codingwebsa@gmail.com>
2025-12-09 14:00:58 +0600

    feat: add hotkeys to navigation

--- apps/web/src/components/navbar/navbar.tsx
+++ apps/web/src/components/navbar/navbar.tsx
@@ -1,35 +1,62 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { useTheme } from "next-themes";
 
+import { useHotkeys } from "~/hooks/use-hotkey/use-hotkey";
 import useMounted from "~/hooks/use-mounted";
 import { cn } from "~/lib/utils";
 
 import { Icons } from "../icons";
 
+interface NavItem {
+  title: string;
+  href: string;
+  isActive: boolean;
+  shortcut?: string;
+  hidden?: boolean;
+}
+
 export default function Navbar() {
   const pathname = usePathname();
   const { resolvedTheme, setTheme } = useTheme();
   const mounted = useMounted();
+  const router = useRouter();
 
-  const navItems = [
+  useHotkeys([
+    ["h", () => router.push("/")],
+    ["p", () => router.push("/projects")],
+    ["w", () => router.push("/writing")],
+    ["r", () => router.push("/resume")],
+  ]);
+
+  const navItems: NavItem[] = [
+    {
+      title: "me",
+      href: "/",
+      isActive: false,
+      shortcut: "h",
+      hidden: pathname === "/",
+    },
     {
       title: "projects",
       href: "/projects",
       isActive: pathname === "/projects",
+      shortcut: "p",
     },
     {
       title: "writing",
       href: "/writing",
       isActive: pathname === "/writing",
+      shortcut: "w",
     },
     {
       title: "resume",
       href: "/resume",
       isActive: pathname === "/resume",
+      shortcut: "r",
     },
   ];
 
@@ -37,16 +64,13 @@ export default function Navbar() {
     setTheme(resolvedTheme === "dark" ? "light" : "dark");
   }
 
-  const isHome = pathname === "/";
-
   return (
     <nav className="absolute top-0 right-0 z-10">
       <div className="px-2 py-2 pr-6 pt-6">
         <div className="flex items-center justify-center gap-4">
-          {!isHome && <NavbarItem title="me" href="/" isActive={false} />}
-          {navItems.map((item) => (
-            <NavbarItem key={item.title} {...item} />
-          ))}
+          {navItems.map((item) => {
+            return <NavbarItem key={item.title} {...item} />;
+          })}
           <button onClick={toggleTheme} title="Toggle theme">
             {mounted && resolvedTheme === "dark" ? (
               <Icons.sun className="text-muted-foreground" />
@@ -60,15 +84,8 @@ export default function Navbar() {
   );
 }
 
-function NavbarItem({
-  title,
-  href,
-  isActive,
-}: {
-  title: string;
-  href: string;
-  isActive: boolean;
-}) {
+function NavbarItem({ title, href, isActive, shortcut, hidden }: NavItem) {
+  if (hidden) return;
   return (
     <Link href={href}>
       <button
@@ -78,6 +95,9 @@ function NavbarItem({
           isActive && "text-primary"
         )}
       >
+        <span className="font-mono text-muted-foreground/80 text-sm">
+          [{shortcut}]
+        </span>{" "}
         {title}
       </button>
     </Link>
