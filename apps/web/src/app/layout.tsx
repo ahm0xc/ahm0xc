28e4b1471100ade120138fb6c642f78816277f79
Ahmed <codingwebsa@gmail.com>
2025-12-11 19:59:11 +0600

    feat: integrate vercel/speed-insights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Pixelify_Sans } from "next/font/google";
 
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistSans } from "geist/font/sans";
 
 import { Footer } from "~/components/footer";
@@ -40,6 +41,7 @@ export default function RootLayout({
         )}
       >
         <Analytics />
+        <SpeedInsights />
         <TRPCReactProvider>
           <ThemeProvider>
             <Navbar />
