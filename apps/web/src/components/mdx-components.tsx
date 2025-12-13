8ddfb416b85f0177a3c7019f321f2db329e4f028
Ahmed <codingwebsa@gmail.com>
2025-12-13 17:17:46 +0600

    feat: add image component for mdx

--- apps/web/src/components/mdx-components.tsx
+++ apps/web/src/components/mdx-components.tsx
@@ -1,3 +1,13 @@
 export const mdxComponents = {
   hr: () => <hr className="my-8 border-t border-foreground/5" />,
+  Image,
 };
+
+interface ImageProps {
+  src: string;
+}
+
+function Image(props: ImageProps) {
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src={props.src} className="rounded-2xl" alt="" />;
+}
