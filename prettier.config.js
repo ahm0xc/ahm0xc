62c83421cdb6744579a43d9ec2d42c636c3cf35b
Ahmed <codingwebsa@gmail.com>
2026-02-17 10:17:31 +0600

    cohere: update deps & fix prettier tailwind config

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,15 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: [
-    "prettier-plugin-tailwindcss",
     "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
   ],
   tabWidth: 2,
   semi: true,
   singleQuote: false,
   trailingComma: "es5",
+  tailwindStylesheet: "./apps/web/src/styles/globals.css",
+  tailwindFunctions: ["clsx", "cn"],
   importOrder: [
     "^(bun:(.*)$)|^(bun$)",
     "^(node:(.*)$)|^(node$)",
